8b919a0973d3c6a43abd3ada302ff7a1bbc9e0f7
Utkin Anton <kerino89@gmail.com>
2021-06-02 19:24:41 +0300

    Create use-update-effect.test.ts

---
+++ test/hooks/use-update-effect.test.ts
@@ -0,0 +1,28 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { useUpdateEffect } from "@hooks/use-update-effect";
+
+describe("Hooks", () => {
+  describe("useUpdateEffect", () => {
+    it("run effect on update", () => {
+      const fn = jest.fn();
+      const { rerender } = renderHook(() => useUpdateEffect(fn));
+
+      expect(fn).not.toHaveBeenCalled();
+
+      rerender();
+
+      expect(fn).toHaveBeenCalledTimes(1);
+    });
+
+    it("run function on unmount", () => {
+      const unmountFn = jest.fn();
+      const fn = jest.fn().mockReturnValue(unmountFn);
+      const { rerender, unmount } = renderHook(() => useUpdateEffect(fn));
+
+      rerender();
+      unmount();
+
+      expect(unmountFn).toHaveBeenCalledTimes(1);
+    });
+  });
+});
