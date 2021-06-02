068d6b31f6a1d3ea972d1ec284849528ea6ec007
Utkin Anton <kerino89@gmail.com>
2021-06-02 21:18:49 +0300

    Create use-previous.test.ts

---
+++ test/hooks/use-previous.test.ts
@@ -0,0 +1,29 @@
+import { renderHook } from "@testing-library/react-hooks";
+import { usePrevious } from "@hooks/use-previous";
+
+describe("Hooks", () => {
+  describe("usePrevious", () => {
+    const hook = () => {
+      return renderHook(({ state }) => usePrevious(state), { initialProps: { state: 0 } });
+    };
+
+    it("return undefined on initial", () => {
+      const { result } = hook();
+
+      expect(result.current).toBeUndefined();
+    });
+
+    it("return previous state after each update", () => {
+      const { result, rerender } = hook();
+
+      rerender({ state: 1 });
+      expect(result.current).toBe(0);
+
+      rerender({ state: 2 });
+      expect(result.current).toBe(1);
+
+      rerender({ state: 3 });
+      expect(result.current).toBe(2);
+    });
+  });
+});
