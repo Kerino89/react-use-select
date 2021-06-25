eb5ea4b460ad6fb92b85eb1f8136419e6a446864
Utkin Anton <kerino89@gmail.com>
2021-06-26 00:59:43 +0300

    Fix test

--- test/hooks/use-previous.test.ts
+++ test/hooks/use-previous.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks/dom";
+import { renderHook } from "@testing-library/react-hooks/dom/pure";
 import { usePrevious } from "@hooks/use-previous";
 
 describe("Hooks", () => {
--- test/hooks/use-update-effect.test.ts
+++ test/hooks/use-update-effect.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks/dom";
+import { renderHook } from "@testing-library/react-hooks/dom/pure";
 import { useUpdateEffect } from "@hooks/use-update-effect";
 
 describe("Hooks", () => {
