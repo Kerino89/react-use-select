20ea6808d1636c5355bd421b46a99513c5bd9269
Utkin Anton <kerino89@gmail.com>
2021-06-26 00:56:47 +0300

    Fix "Could not auto-detect a React renderer"

--- test/hooks/use-previous.test.ts
+++ test/hooks/use-previous.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react-hooks/dom";
 import { usePrevious } from "@hooks/use-previous";
 
 describe("Hooks", () => {
--- test/hooks/use-update-effect.test.ts
+++ test/hooks/use-update-effect.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook } from "@testing-library/react-hooks/dom";
 import { useUpdateEffect } from "@hooks/use-update-effect";
 
 describe("Hooks", () => {
