f08d05aa20676f0169336de26cc1047fc273ddc0
Anton <kerino89@gmail.com>
2023-04-22 13:21:33 +0300

    test: update use-select.test.ts

--- test/use-select.test.ts
+++ test/use-select.test.ts
@@ -1,4 +1,4 @@
-import { renderHook } from "@testing-library/react-hooks";
+import { renderHook, act } from "@testing-library/react-hooks";
 import { useSelect, UseSelectProps, UseSelect } from "../src";
 
 describe("useSelect", () => {
@@ -34,5 +34,70 @@ describe("useSelect", () => {
 
     rerender({ value: undefined });
     expect(result.current.state.selected).toEqual([]);
+  });
+
+  it("checking the setSelected method", () => {
+    const { result } = hook();
+    const selected = [
+      { value: 1, label: "1" },
+      { value: 2, label: "2" },
+    ];
+
+    act(() => {
+      result.current.setSelected(selected);
+    });
+
+    expect(result.current.state.selected).toEqual(selected);
+  });
+
+  it("checking the addSelected method", () => {
+    const { result } = hook();
+    const selected = [
+      { value: 1, label: "1" },
+      { value: 2, label: "2" },
+    ];
+
+    act(() => {
+      result.current.addSelected(selected[1]);
+    });
+
+    expect(result.current.state.selected).toEqual(selected);
+  });
+
+  it("checking the removeSelected method", () => {
+    const { result } = hook();
+    const selected = [
+      { value: 1, label: "1" },
+      { value: 2, label: "2" },
+    ];
+
+    act(() => {
+      result.current.setSelected(selected);
+      result.current.removeSelected(1);
+    });
+
+    expect(result.current.state.selected).toEqual([selected[1]]);
+
+    act(() => {
+      result.current.removeSelected(selected[1]);
+    });
+
+    expect(result.current.state.selected).toEqual([]);
+  });
+
+  it("checking the showOptions and hideOptions method", () => {
+    const { result } = hook();
+
+    act(() => {
+      result.current.showOptions();
+    });
+
+    expect(result.current.state.isOpen).toBeTruthy();
+
+    act(() => {
+      result.current.hideOptions();
+    });
+
+    expect(result.current.state.isOpen).toBeFalsy();
   });
 });
